d2d37a88b76f7e49d2b13d9f0f16be77ba62da5e
lputruele <lputruele@gmail.com>
2014-10-06 16:59:32 -0300

    login html

---
+++ login.js
@@ -0,0 +1,8 @@
+<form>
+    <p>Welcome to the 2048 house!
+</form>
+Username: <input type="text" name="user">
+<form>
+Password: <input type="password" name="pwd">
+    <button id=svt type='button' onclick="alert('ok')" >Login</button >
+</form>
