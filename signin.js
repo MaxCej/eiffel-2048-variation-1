bb8dbc0b6fa3b81fdd6da44fb572a20b6ed9733a
abauer <agustinbauer1@gmail.com>
2014-10-07 00:17:46 -0300

    signin javascript

---
+++ signin.js
@@ -0,0 +1,11 @@
+<form>
+    <p>Create a new user!
+</form>
+<form>
+Username: <input type="text" name="user">
+Password: <input type="password" name="pwd">
+    <input type="submit" value="Sign in">
+<form>
+    <input type="submit" value="Cancel">
+<form>
+</form>
