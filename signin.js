f28b04c92dd8109d1988ba5a82b5f28a48243350
lputruele <lputruele@gmail.com>
2014-11-02 19:35:12 -0300

    Revert "modified Signin screen"
    This reverts commit 25b1c4d52a32175cdb4da335707514ce92cd70b7.

--- signin.js
+++ signin.js
@@ -4,8 +4,6 @@
 <form>
 Name: <input type="text" name="name">
 Surname: <input type="text" name="surname">
-</form>
-<form>
 Username: <input type="text" name="nick">
 Password: <input type="password" name="pass">
     <input type="submit" value="Sign in">
